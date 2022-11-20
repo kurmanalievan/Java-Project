b8c788ba61f63f2daf42b9a5253913b7ae5d577e
nursezim <kurmanalieva.nurs@gmail.com>
2022-11-20 21:16:08 +0600

    Button and text area usage

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,28 @@
 import com.shapes.Circle;
 import com.shapes.Rectangle;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Main {
     public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        JButton button = new JButton("Oroz");
+        button.setBounds(50, 100, 50, 50);
+        JTextArea jt = new JTextArea();
+        jt.setBounds(50, 50, 70, 20);
+        frame.add(jt);
+        button.addActionListener(new ActionListener(){
 
-        System.out.println("Hello world!");
-        Rectangle rec = new Rectangle();
-        Circle circ = new Circle();
-        rec.setSize(10, 15);
-        circ.setSize(5);
-
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jt.setText("Oroz durak");
+            }
+        });
+        frame.setSize(300, 300);
+        frame.add(button);
+        frame.setLayout(null);
+        frame.setVisible(true);
 
 
     }
